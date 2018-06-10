ff80bbb98a5be1341da41eac7a98b3e3d9d62957
edeyrnion <matthias.treubrodt@gmail.com>
2018-06-10 23:52:21 +0200

    Imported Fei

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,12 +3,19 @@ using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
+    Animator _animator;
     NavMeshAgent _navMeshAgent;
 
     private void Start()
     {
         MouseManager.Instance.OnClickEnvironment.AddListener(HandleClickEnvironment);
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
     }
 
     void HandleClickEnvironment(Vector3 target, Layer layer)
