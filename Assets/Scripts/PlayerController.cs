b23b1f35bb73909f0e5f00ff5f451dd7981ab62a
edeyrnion <matthias.treubrodt@gmail.com>
2018-06-13 00:04:51 +0200

    Added direct movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     Animator _animator;
     NavMeshAgent _navMeshAgent;
 
+    private bool _isMouseMovement = true;
+
     private void Start()
     {
         MouseManager.Instance.OnLeftClick.AddListener(HandleClickEnvironment);
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+        
+        if (!_isMouseMovement)
+        {
+            ProcessDirectMovement();
+        }
     }
 
     void HandleClickEnvironment(Vector3 target, Layer layer)
@@ -25,4 +32,19 @@ public class PlayerController : MonoBehaviour
             _navMeshAgent.destination = target;
         }
     }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 move = v * camForward + h * Camera.main.transform.right;
+
+        _navMeshAgent.velocity = move * _navMeshAgent.speed;
+    }
+
 }
+
+
